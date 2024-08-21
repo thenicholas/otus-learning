6b5962b97dcf38e7ede2731e1156be81221f183e
Nick <nicholas.the.2nd@gmail.com>
2024-08-21 11:32:55 +0300

    Added autoloading

---
+++ local/lib/Helper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TheNicholas;
+
+class Helper
+{
+    public static function test(): void
+    {
+        echo "Hello, World!";
+    }
+}
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Hello, World!";
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once(__DIR__ . '/../vendor/autoload.php');
+}
